751bc1234c326cfe2dd33dad66473874a17aaa68
mersinvald <me@mkl.dev>
2019-10-11 02:29:08 +0300

    chore(docs): add a few comments

--- src/logger.rs
+++ src/logger.rs
@@ -49,24 +49,27 @@ pub fn init_logger(v_count: u64, is_silent: bool) -> Result<(), failure::Error> 
     logger.format(|fmt, record| {
         let mut with_prefix =
             |record: &log::Record, prefix: &'static str, color: Color, color_whole_line: bool, verbose: bool| {
-                // Set color
+                // Set the default color
                 let mut style = fmt.style();
                 style.set_color(color);
 
-                let span = SPAN.read().unwrap();
-
                 // Write span and prefix
+                let span = SPAN.read().unwrap();
                 style.set_bold(true);
                 write!(fmt, "[{}] ", style.value(span))?;
                 write!(fmt, "{}", style.value(prefix))?;
                 style.set_bold(false);
 
+                // Extended verbosity for TRACE and DEBUG
                 if verbose && record.module_path().is_some() {
+                    // Print module path
                     let path = record.module_path().unwrap();
                     write!(fmt, "{}", style.value(path))?;
+                    // Print line in the file
                     if let Some(line) = record.line() {
                         write!(fmt, ":{}", style.value(line))?;
                     }
+                    // Add some padding to mitigate the formatting issue a bit
                     write!(fmt, "\t")?;
                 }
 
