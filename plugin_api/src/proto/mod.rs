4c0df27de6dd12f8ed17a024f9b8ad2da41e3d2d
mersinvald <me@mkl.dev>
2019-11-17 14:51:13 +0100

    chore(rust): move doc-comment from trait impl block to fn level

--- plugin_api/src/proto/mod.rs
+++ plugin_api/src/proto/mod.rs
@@ -13,6 +13,46 @@ pub struct Version {
     pub semver: Option<semver::Version>,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum NewVersion {
+    Revision(GitRevision),
+    RevisionAndSemver(GitRevision, semver::Version),
+    Semver(semver::Version),
+    SemverReq(semver::VersionReq),
+    String(String),
+}
+
+impl From<String> for NewVersion {
+    /// Parse String into Version
+    ///
+    /// 1. Try to parse semver::VersionReq
+    /// 2. If 1 failed, try to parse semver::Version
+    /// 3. If 2 failed, construct Version::String
+    fn from(s: String) -> Self {
+        if let Ok(v) = s.parse::<semver::VersionReq>() {
+            return NewVersion::SemverReq(v);
+        }
+
+        if let Ok(v) = s.parse::<semver::Version>() {
+            return NewVersion::Semver(v);
+        }
+
+        NewVersion::String(s)
+    }
+}
+
+impl From<semver::Version> for NewVersion {
+    fn from(v: semver::Version) -> Self {
+        NewVersion::Semver(v)
+    }
+}
+
+impl From<semver::VersionReq> for NewVersion {
+    fn from(v: semver::VersionReq) -> Self {
+        NewVersion::SemverReq(v)
+    }
+}
+
 pub type ProjectAndDependencies = (Project, Vec<Project>);
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
