5512a3cf5f7be2590b90a6b61e9173d570cb6fee
mersinvald <me@mkl.dev>
2019-12-12 21:48:41 +0100

    chore: copy SerIter to semanteecore::utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
 use failure::SyncFailure;
+use serde::{Serialize, Serializer};
+use std::cell::RefCell;
 
 pub trait ResultExt<T, E> {
     fn sync(self) -> Result<T, SyncFailure<E>>
@@ -14,5 +16,36 @@ impl<T, E> ResultExt<T, E> for Result<T, E> {
         E: ::std::error::Error + Send + 'static,
     {
         self.map_err(SyncFailure::new)
+    }
+}
+
+// This serde helper struct allows to avoid collecting iterator into serde_json::Value,
+// through consuming iterator in the serialization process directly
+pub struct SerIter<I>(RefCell<I>);
+
+impl<I> From<I> for SerIter<I> {
+    fn from(iter: I) -> Self {
+        SerIter(RefCell::new(iter))
+    }
+}
+
+// Clippy fires false-positive
+#[allow(clippy::while_let_on_iterator)]
+impl<I, T> Serialize for SerIter<I>
+where
+    T: Serialize,
+    I: Iterator<Item = T>,
+{
+    fn serialize<S>(&self, s: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
+    where
+        S: Serializer,
+    {
+        use serde::ser::SerializeSeq;
+        let mut seq = s.serialize_seq(None)?;
+        let mut iter = self.0.borrow_mut();
+        while let Some(item) = iter.next() {
+            seq.serialize_element(&item)?;
+        }
+        seq.end()
     }
 }
